7e9d96910f08ea64132424873b74a491521b0815
Aaron Peters <aaronpeters.nl@gmail.com>
2019-12-02 23:50:53 +0100

    conditional Twitter intents

--- _11ty/filters.js
+++ _11ty/filters.js
@@ -1,4 +1,6 @@
 const { DateTime } = require("luxon"); // Already in eleventy-base-blog
+const rootUrl = require('../_data/metadata.json').url
+
 module.exports = {
   getWebmentionsForUrl: (webmentions, url) => {
     return webmentions.children.filter(entry => entry['wm-target'] === url)
@@ -11,5 +13,14 @@ module.exports = {
   },
   readableDateFromISO: (dateStr, formatStr = "dd LLL yyyy 'at' hh:mma") => {
     return DateTime.fromISO(dateStr).toFormat(formatStr);
+  },
+  generateShareLink: (url, text) => {
+    const shareText = `${text} by @aaronpeters`
+    const shareUrl = `${rootUrl}${url}`
+    return `https://twitter.com/intent/tweet/?text=${encodeURI(shareText)}&amp;url=${encodeURI(shareUrl)}`
+  },
+  generateDiscussionLink: (url) => {
+    const postUrl = `${rootUrl}${url}`
+    return `https://twitter.com/search?f=tweets&src=typd&q=${encodeURI(postUrl)}`
   }
 }
