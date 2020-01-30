8d140af7cec816b8c8c85d83a5615b0f6886a6a8
Aaron Peters <aaronpeters.nl@gmail.com>
2020-01-30 17:18:15 +0100

    FIX: webmentions replies

--- _11ty/filters.js
+++ _11ty/filters.js
@@ -5,6 +5,15 @@ module.exports = {
   getWebmentionsForUrl: (webmentions, url) => {
     return webmentions.children.filter(entry => entry['wm-target'] === url)
   },
+  isOwnWebmention: (webmention) => {
+    const urls = [
+      'https://sia.codes',
+      'https://twitter.com/thegreengreek'
+    ]
+    const authorUrl = webmention.author ? webmention.author.url : false
+    // check if a given URL is part of this site.
+    return authorUrl && urls.includes(authorUrl)
+  },
   size: (mentions) => {
     return !mentions ? 0 : mentions.length
   },
