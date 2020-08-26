684d242611977d669379d0418af08bbd02f63149
Aaron Peters <aaronpeters.nl@gmail.com>
2020-08-26 15:00:34 +0200

    mybiosource testpage

---
+++ _wpo/mybiosource/includes/menu_nav/menu_nav_contents.js
@@ -0,0 +1,56 @@
+var menu_nav_home={divclass:'anylinkmenu', inlinestyle:'width:73px; border:0px solid gray', linktarget:''}
+menu_nav_home.items=[
+]
+
+var menu_nav_products={divclass:'anylinkmenu', inlinestyle:'width:170px', linktarget:''}
+menu_nav_products.items=[
+    ["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;ELISA Kit", "/elisa_kit"],
+    ["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;CLIA Kit", "/clia_kit"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;PCR Kit", "/pcr_kit"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Monoclonal Antibody", "/monoclonal"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Polyclonal Antibody", "/polyclonal"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Secondary Antibody", "/secondary"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Antigen", "/antigen"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Biochemical", "/biochemical"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;cDNA Clone", "/cdna"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;siRNA", "/siRNA"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Peptide", "/peptide"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Rec./Purified Protein", "/protein"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Antibody Pairs (Cap/Det)", "/antibody_pairs&#38;fc=A"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Antibody & Peptide Pairs", "/antibody_blocking_peptide_pairs&#38;fc=A"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Phospho Antibodies", "/phospho_specific_antibody"],
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Products by Disease", "/diseases"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Products by Pathway", "/pathways"], 
+    ["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Products by Tissue", "/tissues"]
+]
+
+var menu_nav_ordering={divclass:'anylinkmenu', inlinestyle:'width:150px', linktarget:''}
+menu_nav_ordering.items=[
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;How to Order", "/ordering_faqs&question=place_order"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Order Form", "/order_form"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Ordering FAQs", "/ordering_faqs"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Terms & Conditions", "/terms_conditions"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Distributors", "/distributors"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;My Account", "account.php"]
+]
+
+var menu_nav_company={divclass:'anylinkmenu', inlinestyle:'width:135px', linktarget:''}
+menu_nav_company.items=[
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;About Us", "/about_us"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Contact Us", "/contact_us"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Careers", "/careers"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Mailing List", "/mailing_list"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Company FAQs", "/company_faqs"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Our Scholarship", "/scholarship"]
+]
+
+var menu_nav_help={divclass:'anylinkmenu', inlinestyle:'width:150px', linktarget:''}
+menu_nav_help.items=[
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Advanced Search", "advanced_search.php"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Ordering FAQs", "/ordering_faqs"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Contact Us", "/contact_us"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;MyAssay\'s Software", "/my_assay_data_analyzing_software"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Distributors", "/distributors"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Privacy Policy", "/privacy_policy"], 
+	["&nbsp;&nbsp;&nbsp;&nbsp;<img src='includes/menu_nav/menu_nav_rightarrow.gif' border='0'>&nbsp;Terms & Conditions", "/terms_conditions"]
+]
