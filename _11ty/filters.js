51001ebeb34ace26ab3ce07bdce9ab2279d226b0
Aaron Peters <aaronpeters.nl@gmail.com>
2024-10-14 11:16:36 +0200

    bye bye webmentions

--- _11ty/filters.js
+++ _11ty/filters.js
@@ -2,34 +2,5 @@ const { DateTime } = require("luxon"); // Already in eleventy-base-blog
 const rootUrl = require('../_data/metadata.json').url
 
 module.exports = {
-  getWebmentionsForUrl: (webmentions, url) => {
-    return webmentions.children.filter(entry => entry['wm-target'] === url)
-  },
-  isOwnWebmention: (webmention) => {
-    const urls = [
-      'https://www.aaronpeters.nl',
-      'https://twitter.com/aaronpeters'
-    ]
-    const authorUrl = webmention.author ? webmention.author.url : false
-    // check if a given URL is part of this site.
-    return authorUrl && urls.includes(authorUrl)
-  },
-  size: (mentions) => {
-    return !mentions ? 0 : mentions.length
-  },
-  webmentionsByType: (mentions, mentionType) => {
-    return mentions.filter(entry => !!entry[mentionType])
-  },
-  readableDateFromISO: (dateStr, formatStr = "dd LLL yyyy 'at' hh:mma") => {
-    return DateTime.fromISO(dateStr).toFormat(formatStr);
-  },
-  generateShareLink: (url, text) => {
-    const shareText = `${text} by @aaronpeters`
-    const shareUrl = `${rootUrl}${url}`
-    return `https://twitter.com/intent/tweet/?text=${encodeURI(shareText)}&url=${encodeURI(shareUrl)}`
-  },
-  generateDiscussionLink: (url) => {
-    const postUrl = `${rootUrl}${url}`
-    return `https://twitter.com/search?f=tweets&src=typd&q=${encodeURI(postUrl)}`
-  }
+  
 }
